16dac09e057c86f1e08c462e25ef8c6d0200d2d8
Jaime Sanchez <40275727+Sanchezjaime@users.noreply.github.com>
2020-04-21 21:06:49 -0700

    Add files via upload

--- oop_game-v2/js/Game.js
+++ oop_game-v2/js/Game.js
@@ -1,3 +1,44 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Game.js */
+ class Game {
+   constructor() {
+     this.missed = 0;
+     this.phrases = this.createPhrase();
+     this.activePhrase = 'null';
+   }
+
+   /**
+   * Begins game by selecting a random phrase and displaying it to user
+   */
+   startGame() {
+     document.getElementById("overlay").style.diplay = "none";
+     this.activePhrase = this.getRandomPhrase();
+     this.activePhrase.addPhraseToDisplay();
+   };
+
+   /**
+   *Creates phrases for use in Game.
+   *@return {array} An array of phrases that could be used in the game.
+   */
+   createPhrase() {
+     return [
+       new Phrase('Bite the bullet'),
+       new Phrase('Better late than never'),
+       new Phrase('A dime a dozen'),
+       new Phrase('Its not rocket science'),
+       new Phrase('Easy does it')
+     ]
+   };
+
+   /**
+   * Selects random phrase from phrases property
+   * @return {Object} Phrase object chosen to be used
+   */
+   getRandomPhrase() {
+     return this.phrases[Math.floor(Math.random()*this.phrases.length)];
+   };
+
+
+
+ }
--- oop_game-v2/js/Phrase.js
+++ oop_game-v2/js/Phrase.js
@@ -1,3 +1,34 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * Phrase.js */
+class Phrase {
+   constructor(phrase) {
+     this.phrase = phrase;
+   }
+
+   /**
+   * Display phrase on game board
+   */
+   addPhraseToDisplay() {
+     let splitPhrase = this.phrase.split('');
+     const phraseId = document.getElementById('phrase');
+     splitPhrase.forEach((letter) => {
+       if (letter !== ' ') {
+         let characterKey = document.createElement('li');
+         characterKey.textContent = letter;
+         characterKey.setAttribute('class', `hide letter ${characterKey.textContent}`);
+         phraseId.firstElementChild.appendChild(characterKey);
+       } else {
+         let space = document.createElement('li');
+         space.textContent = ` `;
+         space.setAttribute('class', `space`);
+         phraseId.firstElementChild.appendChild(space);
+       }
+     });
+   }
+
+   //checkLetter() {}
+   //showMatchedLetter() {}
+
+
+ }
